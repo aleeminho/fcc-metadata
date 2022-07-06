735b79dd902cea86e666975e146a9adcb6a38ee1
aleeminho <a.fikri2411@gmail.com>
2022-07-06 15:17:41 +0700

    make any changes

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname: name, mimetype: type, size } = req.file
+  const file = {
+    name,
+    type,
+    size
+  }
+  res.json(file)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
